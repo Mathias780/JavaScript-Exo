8bf704d48f89a9347911e7abf73ee3acdf355a41
Habti Mathias <Habti.mathias@gmail.com>
2025-11-14 12:59:45 +0100

    [New] exo_3 comment added

--- Arays/exo_3.js
+++ Arays/exo_3.js
@@ -1,5 +1,5 @@
 let brands = ["nike","cahartt","uniqlo"]
 
-for (let brand of  brands) {
+for (let brand of brands) { // take directly each of the index of the choosen array so it display them like one by one
     console.log(brand)
 }
